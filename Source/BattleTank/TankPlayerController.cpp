864fae30033db3f6fccedfd0850bb2ba135a2cd4
wesleyearlstander <wesleyearlstander@gmail.com>
2020-04-15 18:45:31 +0200

    Code Refactor

---
+++ Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,9 @@
+// Copyright Wesley Earl Stander 2020
+
+#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
---
+++ Source/BattleTank/TankPlayerController.h
@@ -0,0 +1,21 @@
+// Copyright Wesley Earl Stander 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+
+public:
+	ATank* GetControlledTank() const;
+};
