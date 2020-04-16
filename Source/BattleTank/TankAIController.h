f9d4efd23aa98ad85cd26995fda0b2d4ac9407f5
wesleyearlstander <wesleyearlstander@gmail.com>
2020-04-16 17:42:27 +0200

    removedLFS

---
+++ Source/BattleTank/TankAIController.h
@@ -0,0 +1,17 @@
+// Copyright Wesley Earl Stander 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "TankAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
